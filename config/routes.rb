1e188d01c633762f4bd83352986e9af6628c57b5
leonardo fukuje <leonardo.fukuje@gmail.com>
2022-07-30 12:14:31 -0300

    added photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users
+  # devise_for :users
+  #line above was replaced by the three lines below
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   resources :services, only: [:index, :new, :create] do
     resources :orders, only: [:index, :new, :create]
