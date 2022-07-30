89155ee7a3f38a0e2ec0a8cd46ee1314bd4ee97b
leonardo fukuje <leonardo.fukuje@gmail.com>
2022-07-30 16:11:30 -0300

    added service page

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,5 @@
 class Service < ApplicationRecord
   SPECIALTIES = ["Front-end", "Back-end", "Data scientist", "Product manager", "Devops engineer", "Data engineer", "Software engineer"  ]
-
   belongs_to :user
   has_many :orders
 end
