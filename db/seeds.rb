b3370c04206353179dc1df4f8c9286178d78fb96
Mateus Ferreira <mateusferreira0704@gmail.com>
2022-08-06 11:57:46 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # seed de usuário full-name e-mail senha e imagem do usuário
 # seed de serviço - descrição preço e especialidade
-case Rails.env
-  when 'development'
-    puts 'Creating 20 fake'
-    20.times do
-      avatar = %w[avatar1.png avatar2.png avatar3.png avatar4.png avatar5.png avatar6.png avatar7.png avatar8.png avatar9.png avatar10.png avatar11.png avatar12.png
-                avatar13.png avatar14.png avatar15.png avatar16.png avatar17.png avatar18.png avatar19.png avatar20.png].sample
+puts 'Creating 20 fake'
+20.times do
+  avatar = %w[avatar1.png avatar2.png avatar3.png avatar4.png avatar5.png avatar6.png avatar7.png avatar8.png avatar9.png avatar10.png avatar11.png avatar12.png
+            avatar13.png avatar14.png avatar15.png avatar16.png avatar17.png avatar18.png avatar19.png avatar20.png].sample
 
-      user = User.create(
-        full_name: Faker::Name.name,
-        email: Faker::Internet.email,
-        password: "12345678"
-      )
+  user = User.create(
+    full_name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "12345678"
+  )
 
-      user.photo.attach(io: File.open(File.join(Rails.root, "app/assets/images/avatar/#{avatar}")), filename: "#{avatar}")
+  user.photo.attach(io: File.open(File.join(Rails.root, "app/assets/images/avatar/#{avatar}")), filename: "#{avatar}")
 
-      service = Service.create(
-        specialty: ["Front-end", "Back-end", "Product-Manager", "Devops", "Data-Engineer", "Software-Engineer"].sample,
-        description: ["10 anos de experiencia, formado no batch #566","5 anos de experiencia, formado no batch #366", "2 anos de experiencia, formado no batch #756",
-          "1 ano de experiencia, formado no batch #840", "7 anos de experiencia, formado no batch #496", "4 anos de experiencia, formado no batch #500",
-          "8 anos de experiencia, formado no batch #250", "6 anos de experiencia, formado no batch #463", "10 anos de experiencia, formado no batch #566",
-          "6 meses de experiencia, formado no batch #860"].sample,
-        price: (50..280).to_a.sample,
-        user_id: user.id
-      )
-    end
-  puts 'Finished!'
+  service = Service.create(
+    specialty: ["Front-end", "Back-end", "Product-Manager", "Devops", "Data-Engineer", "Software-Engineer"].sample,
+    description: ["10 anos de experiencia, formado no batch #566","5 anos de experiencia, formado no batch #366", "2 anos de experiencia, formado no batch #756",
+      "1 ano de experiencia, formado no batch #840", "7 anos de experiencia, formado no batch #496", "4 anos de experiencia, formado no batch #500",
+      "8 anos de experiencia, formado no batch #250", "6 anos de experiencia, formado no batch #463", "10 anos de experiencia, formado no batch #566",
+      "6 meses de experiencia, formado no batch #860"].sample,
+    price: (50..280).to_a.sample,
+    user_id: user.id
+  )
 end
+puts 'Finished!'
